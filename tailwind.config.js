c9a8e48c0618de5832ab67cb686a1d54106c9343
Ashik-Himel <ashikujjamanhimel@gmail.com>
2024-09-14 01:07:35 +0600

    3 sections designed of homepage

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+        md: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        'md': '6rem',
+      }
+    },
+    extend: {
+      fontFamily: {
+        inter: "'Inter', sans-serif",
+        raleway: "'Raleway', sans-serif"
+      },
+      colors: {
+        "primary": "#4E47FF",
+        "secondary": "#5B54FF",
+        "bg-color": "#F5F8FF",
+        "dark-1": "#252432",
+        "dark-2": "#8987A1",
+      }
+    },
+  },
+  plugins: [],
+}
+
